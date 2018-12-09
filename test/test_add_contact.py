4cda7c3c353aefcb0987876a86fd0f4125c8bf05
SergNot <s.notevskiy@gmail.com>
2018-12-09 20:10:48 +0200

    выделен класс для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,20 +11,20 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="aasfasfasf", middlename="asfasfasafs", lastname="asdfasdfsdfasdf",
-                            nickname="asdfasdf", title="asvdsadv", company="czvsavwacvsd", address="sadvwrevsv",
-                            home="1231231233", mobile="3213213213", work="1233211233", fax="1233211233",
-                            email="hfhwfwb@habdhfa.ru", email2="hbsadlhi@lbhal.ru", email3="bsalhdhflsdbflh@basdflhsdk.ru",
-                            homepage="www.bibuhbubu.ru", address2="fsadfsadfsdfsdf", phone2="asdfsadfasdfasdfsdfs",
-                            notes="asdfasdfsadfsadfasdfasd"))
+    app.contact.create(Contact(firstname="aasfasfasf", middlename="asfasfasafs", lastname="asdfasdfsdfasdf",
+                               nickname="asdfasdf", title="asvdsadv", company="czvsavwacvsd", address="sadvwrevsv",
+                               home="1231231233", mobile="3213213213", work="1233211233", fax="1233211233",
+                               email="hfhwfwb@habdhfa.ru", email2="hbsadlhi@lbhal.ru", email3="bsalhdhflsdbflh@basdflhsdk.ru",
+                               homepage="www.bibuhbubu.ru", address2="fsadfsadfsdfsdf", phone2="asdfsadfasdfasdfsdfs",
+                               notes="asdfasdfsadfsadfasdfasd"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="",
-                            nickname="", title="", company="", address="",
-                            home="", mobile="", work="", fax="",
-                            email="", email2="", email3="",
-                            homepage="", address2="", phone2="",
-                            notes=""))
+    app.contact.create(Contact(firstname="", middlename="", lastname="",
+                               nickname="", title="", company="", address="",
+                               home="", mobile="", work="", fax="",
+                               email="", email2="", email3="",
+                               homepage="", address2="", phone2="",
+                               notes=""))
     app.session.logout()
