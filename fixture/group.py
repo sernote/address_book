08d6ff47774e2533b4406ad6cd5a327539d7b80a
SergNot <s.notevskiy@gmail.com>
2018-12-10 21:17:10 +0200

    рефакторинг. переменные для групп и контактов теперь с маленькой буквы.

--- fixture/group.py
+++ fixture/group.py
@@ -8,7 +8,7 @@ class Grouphelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, Group):
+    def create(self, group):
         wd = self.app.wd
         self.open_group_page()
         # init group creation
@@ -16,18 +16,18 @@ class Grouphelper:
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(Group.name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(Group.header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def edit_first(self, Group):
+    def edit_first(self, group):
         wd = self.app.wd
         self.open_group_page()
         # выбрать первую группу
@@ -37,13 +37,13 @@ class Grouphelper:
         #ввод новых давнных в поля
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(Group.name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(Group.header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         #подтвердить редактирование
         wd.find_element_by_name('update').click()
         self.return_to_groups_page()
