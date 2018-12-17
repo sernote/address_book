750df5e656c9c91c26053c644e5702b006f6236f
SergNot <s.notevskiy@gmail.com>
2018-12-17 23:25:29 +0200

    добавлена валидация фикстуры перед каждым текстом логаут перенесен в конец сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,11 +2,7 @@
 from model.group import Group
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name ="asdasdas", header="asdasdasd", footer="asdasdasd"))
-    app.session.logout()
+    app.group.create(Group(name="asdasdas", header="asdasdasd", footer="asdasdasd"))
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name ="", header="", footer=""))
-    app.session.logout()
+    app.group.create(Group(name="", header="", footer=""))
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,31 +1,21 @@
 from model.contact import Contact
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(firstname="editname", lastname="editlastname",
                                    address="editaddress",
                                    home="1111111", mobile="2222222", work="3333333", email="edit@email.ru",
                                    email2="edit2@email.ru", email3="edit3@basdflhsdk.ru",
                                    address2="edit2address", phone2="123123123"))
-    app.session.logout()
 
 def test_edit_contact_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(firstname="Editname", lastname="Editlastname"))
-    app.session.logout()
 
 def test_edit_contact_address(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(address="editaddress"))
-    app.session.logout()
 
 def test_edit_contact_phones(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(home="1111111", mobile="2222222", work="3333333", phone2="123123123"))
-    app.session.logout()
 
 def test_edit_contact_emails(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(email="edit@email.ru",
                                    email2="edit2@email.ru", email3="edit3@basdflhsdk.ru"))
-    app.session.logout()
