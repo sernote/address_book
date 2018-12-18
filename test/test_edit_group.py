1bda4764810b47ccf627a1a7d9bdf74694fd0dc7
SergNot <s.notevskiy@gmail.com>
2018-12-18 22:02:59 +0200

    добавлено предусловие для теста редактирования групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,13 +1,24 @@
 from model.group import Group
 
 def test_edit_first_group(app):
-    app.group.edit_first(Group(name ="afteredit", header="hedafteredit", footer="footafteredit"))
+    if app.group.count() == 0:
+        app.group.create(Group(name='Testname',header="testhead", footer="testfoot"))
+    app.group.edit_first(Group(name="afteredit", header="hedafteredit", footer="footafteredit"))
+
 
 def test_edit_group_name(app):
-    app.group.edit_first(Group(name ="Edit group"))
+    if app.group.count() == 0:
+        app.group.create(Group(name='Testname'))
+    app.group.edit_first(Group(name="Edit group"))
+
 
 def test_edit_group_header(app):
-    app.group.edit_first(Group(header ="Edit header"))
+    if app.group.count() == 0:
+        app.group.create(Group(header="testhead"))
+    app.group.edit_first(Group(header="Edit header"))
+
 
 def test_edit_group_footer(app):
-    app.group.edit_first(Group(footer ="Edit footer"))
+    if app.group.count() == 0:
+        app.group.create(Group(footer="testfoot"))
+    app.group.edit_first(Group(footer="Edit footer"))
