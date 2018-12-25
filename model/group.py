36e6463bb7def97529ab49dd2a1f386545dec232
SergNot <s.notevskiy@gmail.com>
2018-12-25 23:40:17 +0200

    добавлено сравнение списков до и после удаления в тесте для групп

--- model/group.py
+++ model/group.py
@@ -7,4 +7,7 @@ class Group:
         self.id= id
 
     def __repr__(self):
-        return ""
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
