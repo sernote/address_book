8aa4956931d52d2f7a732c6d5e9689ef48ffbfe1
SergNot <s.notevskiy@gmail.com>
2018-12-25 23:33:39 +0200

    добавлены проверки на длину списков для тестов групп

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,4 @@
-
+from model.group import Group
 
 class Grouphelper:
     def __init__(self, app):
@@ -64,4 +64,17 @@ class Grouphelper:
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name('selected[]'))
+
+    def get_groups_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector('span.group'):
+            text = element.text
+            id= element.find_element_by_name('selected[]').get_attribute('value')
+            groups.append(Group(name=text,id=id))
+        return groups
+
+
+
 
