788ada263d3bd8720a4924e3224303384e23773a
SergNot <s.notevskiy@gmail.com>
2018-12-29 00:14:27 +0200

    добавлено хеширование для теста добавления групп контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,8 +10,8 @@ def test_add_contact(app):
                                homepage="www.bibuhbubu.ru", address2="fsadfsadfsdfsdf", phone2="asdfsadfasdfasdfsdfs",
                                notes="asdfasdfsadfsadfasdfasd")
     app.contact.create(contact)
+    assert len(old_contacts)+1 == app.contact.count()
     new_contacts = app.contact.get_contacts_list()
-    assert len(old_contacts)+1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
