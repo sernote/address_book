91cfddb1c8a9064dacf682e8fc280fd58d4d7bcf
SergNot <s.notevskiy@gmail.com>
2019-01-06 01:08:54 +0200

    добавлена загрузка тестовых данных из файла json для контактов

---
+++ data/contacts.py
@@ -0,0 +1,17 @@
+from model.contact import Contact
+
+
+
+testdata = [
+    Contact(firstname="editname", lastname="editlastname",
+                                   address="editaddress",
+                                   home="1111111", mobile="2222222", work="3333333", email="edit@email.ru",
+                                   email2="edit2@email.ru", email3="edit3@basdflhsdk.ru",
+                                   address2="edit2address", phone2="123123123"),
+    Contact(firstname="editname2", lastname="editlastname2",
+            address="editaddress2",
+            home="11111112", mobile="2222222", work="33333332", email="edit2@email.ru",
+            email2="edit2@email.ru", email3="edit3@basdflhsdk.ru",
+            address2="edit2address", phone2="123123123"),
+
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + ' '*10
-    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="", home="", mobile="", work="", email="", email2="", email3="",
-                     phone2="")] + [
-    Contact(firstname=random_string('first', 10), lastname=random_string('last', 10), address=random_string('address', 30),
-            home=random_string('home', 15), mobile=random_string('mobile', 15), work=random_string('work', 15),
-            phone2=random_string('phone2', 15), email=random_string('email', 20), email2=random_string('email2', 20),
-            email3=random_string('email3', 20))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.create(contact)
     assert len(old_contacts)+1 == app.contact.count()
