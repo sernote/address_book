afe98c5cb03f6c683997528021a1884b404a65b6
SergNot <s.notevskiy@gmail.com>
2019-01-29 21:32:24 +0200

    фикстура для работы с БД

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,6 +1,6 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='127.0.0.1', database='addressbook', user='root', password='')
+connection = pymysql.connect(host='localhost', database='addressbook', user='root', password='')
 try:
     cursor = connection.cursor()
     cursor.execute('select * from group_list')
