071d651aae8334e3add2144dfea3a944da753313
SergNot <s.notevskiy@gmail.com>
2019-01-30 22:35:46 +0200

    тестовый тест с базой данных

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,5 @@
 import pymysql.cursors
+from model.group import  Group
 
 
 class DbFixture:
@@ -12,3 +13,15 @@ class DbFixture:
 
     def destroy(self):
         self.connection.close()
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute('select group_id, group_name, group_header, group_footer from group_list')
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+                return list
+        finally:
+            cursor.close()
