4182bfecd66b0da4076a8155768ccbc951f99987
SergNot <s.notevskiy@gmail.com>
2019-02-04 22:10:46 +0200

    тест редактирования групп переведен на работу с БД добавлена проверка через ui по опции check_ui

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,17 +2,21 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_some_group(app, json_groups):
-    if app.group.count() == 0:
-        app.group.create(Group(name='Testname',header="testhead", footer="testfoot"))
-    old_groups = app.group.get_groups_list()
+def test_edit_some_group(app, db, json_groups, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name='Testname', header="testhead", footer="testfoot"))
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     group = json_groups
     group.id = old_groups[index].id
     app.group.edit_by_index(index, group)
-    new_groups = app.group.get_groups_list()
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
+    print(old_groups[index])
     old_groups[index] = group
+    print(old_groups[index])
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
 
 
